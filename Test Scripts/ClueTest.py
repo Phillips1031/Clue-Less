53fc95f14ef3763e67db7eeb3192a6bc3a514a40
Erika <Erika@Erika-PC>
2017-11-17 00:17:40 -0800

    Trying this again...

---
+++ Test Scripts/ClueTest.py
@@ -0,0 +1,26 @@
+'''
+Created on Nov 16, 2017
+
+@author: Erika
+'''
+
+from Clues import ClueDeck
+
+
+if __name__ == '__main__':
+    
+    
+    deck = ClueDeck.ClueDeck()
+    print( deck.hasCards() )
+    
+
+    FE = deck.dealFinalEvidence()
+    for card in FE:
+        print( card )
+    for i in range(0,19):
+        print( deck.dealClue() )
+    
+   
+    
+    
+    
